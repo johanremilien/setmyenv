e3f8bab34ce90b15640c5884655e45893add69df
Johan, Axel REMILIEN <johan.remilien@gmail.com>
2023-02-24 15:38:00 +0100

    Update files

--- .shortcuts.sh
+++ .shortcuts.sh
@@ -21,7 +21,7 @@ alias zsh_reload="source ~/.zshrc"
 
 # directory & file
 alias rmdir="rm -rf"
-function mkcd { mkdir $1; cd $_ }
+function mkcd { mkdir -p $1; cd $_ }
 function swap {
 	first=$1
 	second=$2
@@ -32,4 +32,47 @@ function swap {
 		exit 0
 	fi
 	exit 1
+}
+
+# convertions
+function seconds_to_time {
+	totalSeconds=$1
+
+	d=$((totalSeconds/60/60/24))
+	h=$((totalSeconds/60/60%24))
+	m=$((totalSeconds/60%60))
+	s=$((totalSeconds%60))
+
+	local time
+
+	(( $d )) && time+="$d day$( (( d > 1 )) && echo s) "
+	(( $h )) && time+="$h hour$( (( h > 1 )) && echo s) "
+	(( $m )) && time+="$m minute$( (( m > 1 )) && echo s) "
+	time+="$s second$( (( s > 1 )) && echo s)"
+
+	echo $time
+}
+
+# echo
+function echo_bold { echo -e "\033[1m$@\033[0m" }
+function echo_vars { for var in $@; do echo "$var=${(P)var}"; done }
+function echo_exec {
+	set +e
+	echo_bold "Starting: \"$@\""
+
+	time=`date +%s`
+	$@; ret=$?
+	timeEnd=`date +%s`
+
+	runtime=$((timeEnd-time))
+	((totaltime+=$runtime))
+
+	if [ $ret -eq 0 ]; then
+		echo_bold "The process \"$@\" exited normally ($(seconds_to_time $runtime))"
+	else
+		echo_bold "The process \"$@\" failed ($(seconds_to_time $runtime))"
+	fi
+
+	set -e
+	return $ret
 }
